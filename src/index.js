6f48e76916ba3e3d2f66a4c902e259e2c531e0e9
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-07-04 13:45:48 +0300

    chore: enable the use of cors in fastify

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import Fastify from 'fastify';
 import mercurius from 'mercurius';
+import cors from '@fastify/cors';
+
 import { schema } from './graphql/schema.js';
 import { resolvers } from './graphql/resolvers.js';
 
 const fastify = Fastify({
   logger: true,
 });
+
+await fastify.register(cors);
 
 fastify.register(mercurius, {
   schema: schema,
